c7986ecb91fa1fc363a266294ae00ba9ce97ba37
colelemahieu <colelemahieu@ku.edu>
2021-03-31 13:11:43 -0400

    get muon number, pt

--- plot.C
+++ plot.C
@@ -16,11 +16,13 @@ void plot()
   TTree *evt_tree = (TTree*)input->Get("ggHiNtuplizer/EventTree");
 
   int muontrigger;
+  int muon_num;
   vector<Float_t> *mu_pt;
   //trig_tree->SetBranchAddress("HLT_HIUPC_SingleMuOpen_NotMBHF2AND_v1",&muontrigger);
   TBranch*b_mupt;
   mu_pt=0;
   evt_tree->SetBranchAddress("muPt", &mu_pt, &b_mupt);
+  evt_tree->SetBranchAddress("nMu", &muon_num);
 
   int entries = evt_tree->GetEntries();
   cout << entries << endl;
@@ -35,8 +37,15 @@ void plot()
 	{
 	  //cout << mu_pt << endl;
 	}
-      //cout<<mu_pt->size()<<endl;
-      for (unsigned int muIter=0; muIter<mu_pt->size(); muIter++)cout<< "muPt=:" << mu_pt->at(muIter)<<endl;
+      
+      if (muon_num > 0)
+	{
+	  cout << muon_num << endl;
+	  for (unsigned int muIter=0; muIter<mu_pt->size(); muIter++)
+	    {
+	      cout<< "muPt=:" << mu_pt->at(muIter)<<endl;
+	    }
+	}
     }
 
   
